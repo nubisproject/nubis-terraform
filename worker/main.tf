8f7a9305062cf5e06624ece2b92c581cbca6b47b
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-09-12 14:56:45 -0400

    add default ports tcp/22 and tcp/80 to instances

--- worker/main.tf
+++ worker/main.tf
@@ -23,6 +23,27 @@ resource "aws_security_group" "extra" {
     Shutdown       = "never"
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   lifecycle {
     create_before_destroy = true
   }
