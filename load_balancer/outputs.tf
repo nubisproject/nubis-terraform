59e871ca94083df8bd0b89062d05af87662c8266
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-09-13 09:26:57 -0400

    Expose ELB zone_id

--- load_balancer/outputs.tf
+++ load_balancer/outputs.tf
@@ -10,6 +10,10 @@ output "ipv6_address" {
   value = "ipv6.${aws_elb.load_balancer.dns_name}"
 }
 
+output "zone_id" {
+  value = "${aws_elb.load_balancer.zone_id}"
+}
+
 output "name" {
   value = "${aws_elb.load_balancer.name}"
 }
