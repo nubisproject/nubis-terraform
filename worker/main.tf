2db2973f04237f4c966afcd8afa6dfaa7524d17a
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-04-25 11:35:19 -0400

    terraform fmt

--- worker/main.tf
+++ worker/main.tf
@@ -227,17 +227,17 @@ data "template_file" "user_data" {
   template = "${file("${path.module}/templates/userdata.tpl")}"
 
   vars {
-    NUBIS_STACK       = ""
-    NUBIS_PROJECT     = "${var.service_name}"
-    CONSUL_ACL_TOKEN  = "${var.consul_token}"
+    NUBIS_STACK           = ""
+    NUBIS_PROJECT         = "${var.service_name}"
+    CONSUL_ACL_TOKEN      = "${var.consul_token}"
     NUBIS_TECHNICAL_OWNER = "${var.technical_owner}"
-    NUBIS_PURPOSE     = "${var.purpose}"
-    NUBIS_ENVIRONMENT = "${var.environment}"
-    NUBIS_DOMAIN      = "${var.nubis_domain}"
-    NUBIS_ACCOUNT     = "${var.account}"
-    NUBIS_STACK       = "${var.service_name}-${var.environment}"
-    NUBIS_SUDO_GROUPS = "${var.nubis_sudo_groups}"
-    NUBIS_USER_GROUPS = "${var.nubis_user_groups}"
+    NUBIS_PURPOSE         = "${var.purpose}"
+    NUBIS_ENVIRONMENT     = "${var.environment}"
+    NUBIS_DOMAIN          = "${var.nubis_domain}"
+    NUBIS_ACCOUNT         = "${var.account}"
+    NUBIS_STACK           = "${var.service_name}-${var.environment}"
+    NUBIS_SUDO_GROUPS     = "${var.nubis_sudo_groups}"
+    NUBIS_USER_GROUPS     = "${var.nubis_user_groups}"
   }
 }
 
