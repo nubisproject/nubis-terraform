4a0e4c9ad37917306ffc16767fa9453d75c3c382
eziegenhorn <eric@ziegenhorns.com>
2017-08-25 09:55:20 -0500

    Remove deprecated name

--- mail/consul.tf
+++ mail/consul.tf
@@ -17,21 +17,18 @@ provider "consul" {
 # Publish our outputs into Consul for our application to consume
 resource "consul_keys" "config" {
   key {
-    name   = "smtp_user"
     path   = "${module.consul.config_prefix}/SMTP/User"
     value  = "${aws_iam_access_key.email.id}"
     delete = true
   }
 
   key {
-    name   = "smtp_password"
     path   = "${module.consul.config_prefix}/SMTP/Password"
     value  = "${aws_iam_access_key.email.ses_smtp_password}"
     delete = true
   }
 
   key {
-    name   = "smtp_host"
     path   = "${module.consul.config_prefix}/SMTP/Server"
     value  = "email-smtp.${var.region}.amazonaws.com"
     delete = true
