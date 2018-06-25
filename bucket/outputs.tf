d90124969ea3fe6be817bb0505c64b71f11125e1
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-06-25 19:32:50 -0400

    Expose the bucket Route 53 Hosted Zone ID as hosted_zone_id
    Fixes #211

--- bucket/outputs.tf
+++ bucket/outputs.tf
@@ -9,3 +9,7 @@ output "arn" {
 output "website_endpoint" {
   value = "${aws_s3_bucket.bucket.website_endpoint}"
 }
+
+output "hosted_zone_id" {
+  value = "${aws_s3_bucket.bucket.hosted_zone_id}"
+}
